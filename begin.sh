18f89fd6c2b7b3a1d132d986ab3d8a1010909ead
Phu Hoa Doan <57792296+june4th@users.noreply.github.com>
2024-05-27 15:39:19 +0700

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -3,7 +3,7 @@ yes | pkg update && pkg upgrade
 yes | pkg install git build-essential cmake
 git clone https://github.com/xmrig/xmrig.git && apt upgrade -y && cd xmrig
 mkdir build && cd build
-cmake .. -DWITH_HWLOC-OFF
+cmake .. -DWITH_HWLOC=OFF
 make -j10 
 echo "./xmrig -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -a randomx -u NHbJB5KUkEUhp5pAkUYbera7bfdXWKTgjbNE.XMRIG -p x -k --nicehash" > start.sh
 if [ ! -f ~/.bashrc ]; then
